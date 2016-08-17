c0ae2f6ad723b3b56f5bdbb84d33b05871261c86
henio.bez <henio.bez@neoway.com.br>
2016-08-16 22:38:25 -0300

    add sharing action on detail activity fragment

--- app/src/main/java/com/example/henio/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/henio/sunshine/app/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.example.henio.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,7 +19,12 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private final String LOG_CAT = DetailActivityFragment.class.getSimpleName();
+    private ShareActionProvider shareActionProvider;
+    private String forecastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -24,11 +35,38 @@ public class DetailActivityFragment extends Fragment {
 
         Intent intent = getActivity().getIntent();
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            this.forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
             textView.setText(forecastStr);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+        this.shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if(this.shareActionProvider != null){
+            this.shareActionProvider.setShareIntent(this.createShareIntent());
+        } else {
+            Log.d(LOG_CAT, "Error to create share action provide");
+        }
+
+    }
+
+    private Intent createShareIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        //impede que a Activity que está executando seja colocada na pilha de atividades.
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+
+        String extraStr = forecastStr + " #sunshineApp";
+        shareIntent.putExtra(Intent.EXTRA_TEXT, extraStr);
+
+        return shareIntent;
     }
 }
